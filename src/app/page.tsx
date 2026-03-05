0d526afa3e4dd918a85c463ea61f68995a8dd39a
shubham <pawarshubham5959@gmail.com>
2026-03-05 18:27:03 +0530

    setting up trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
-export default function Home() {
+
+const Page = async () =>{
+  const queryClient =getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
   return (
     <div>
-      NexFlow
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading</p>}>
+        <Client/>
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
+
+export default Page;
