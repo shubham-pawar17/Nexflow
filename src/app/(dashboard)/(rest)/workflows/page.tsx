4ccbd2b3724400630eae25d8677978d2721bdfea
shubham <pawarshubham5959@gmail.com>
2026-05-05 17:51:31 +0530

    implement workflows CRUD with Prisma, tRPC, and React Query

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,26 @@
+import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflow";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydarteClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
 
-const Page = async () =>{
+const Page = async () => {
 
     await requireAuth();
-    return <p>Workflow</p>
+    prefetchWorkflows();
+
+    return (
+        <WorkflowsContainer>
+            <HydarteClient>
+                <ErrorBoundary fallback={<p>Error!</p>}>
+                    <Suspense fallback={<p>...Loading</p>}>
+                        <WorkflowsList />
+                    </Suspense>
+                </ErrorBoundary>
+            </HydarteClient>
+        </WorkflowsContainer>
+    )
 };
 
 export default Page;
