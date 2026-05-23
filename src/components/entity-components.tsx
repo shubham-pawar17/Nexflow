5ca63fe113f37880c127adc0e2761c279cf79dd1
shubham <pawarshubham5959@gmail.com>
2026-05-23 23:40:03 +0530

    created ui component

--- src/components/entity-components.tsx
+++ src/components/entity-components.tsx
@@ -1,7 +1,28 @@
-import { PlusIcon, SearchIcon } from "lucide-react";
+import { AlertTriangleIcon, Loader2Icon, MoreVerticalIcon, PackageOpenIcon, PlusIcon, SearchIcon, TrashIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Input } from "./ui/input";
+import {
+    Empty,
+    EmptyContent,
+    EmptyDescription,
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle
+} from "@/components/ui/empty";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardTitle
+} from "./ui/card";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 
 type EntityHeaderProps = {
     title: string;
@@ -100,7 +121,7 @@ export const EntitySearch = ({
     return (
         <div className="relative ml-auto">
             <SearchIcon className="size-3.5 absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"/>
-            <Input className={"max-w-[200px] bg-background shadow-none border-border pl-8"} placeholder={placeholder}
+            <Input className={"bg-background shadow-none border-border pl-8"} placeholder={placeholder}
             value={value}
             onChange={(e) => onChange(e.target.value)}
             />
@@ -146,4 +167,198 @@ export const EntityPagination = ({
             </div>
         </div>
     )
+};
+
+interface StateViewProps {
+    message?: string;
+};
+
+export const LoadingView = ({
+    message
+}: StateViewProps) => {
+    return (
+        <div className="flex justify-center items-center h-full flex-1 flex-col gap-y-4">
+            <Loader2Icon className="size-6 animate-spin text-primary"/>
+            {!!message && (
+                <p className="text-sm text-muted-foreground">
+                    {message}
+            </p>
+            )}
+        </div>
+    );
+};
+
+export const ErrorView = ({
+    message
+}: StateViewProps) => {
+    return (
+        <div className="flex justify-center items-center h-full flex-1 flex-col gap-y-4">
+            <AlertTriangleIcon className="size-6 text-primary"/>
+            {!!message && (
+                <p className="text-sm text-muted-foreground">
+                    {message}
+            </p>
+            )}
+        </div>
+    );
+};
+
+interface EmptyViewProps extends StateViewProps {
+    onNew?: () => void;
+};
+
+export const EmptyView = ({
+    message,
+    onNew
+}: EmptyViewProps) => {
+    return (
+        <Empty className="border border-dashed bg-white">
+            <EmptyHeader>
+                <EmptyMedia>
+                    <PackageOpenIcon/>
+                </EmptyMedia>
+            </EmptyHeader>
+            <EmptyTitle>
+                No Items
+            </EmptyTitle>
+            {!!message && (
+                <EmptyDescription>
+                    {message}
+                </EmptyDescription>
+            )}
+            {!!onNew && (
+                <EmptyContent>
+                    <Button onClick={onNew}>
+                        Add Items
+                    </Button>
+                </EmptyContent>
+            )}
+        </Empty>
+    );
+};
+
+interface EntityListProps <T> {
+    items: T[];
+    renderItems: (item:T, index:number ) => React.ReactNode;
+    getkey?: (item:T, index:number ) => string | number;
+    emptyView?: React.ReactNode;
+    classname?: string;
+};
+
+export function EntityList<T>({
+    items,
+    renderItems,
+    getkey,
+    emptyView,
+    classname
+}: EntityListProps<T>) {
+    if (items.length === 0 && emptyView) {
+        return (
+            <div className="flex-1 flex justify-center">
+                <div className="max-w-sm-auto">
+                    {emptyView}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className={cn(
+            "flex flex-col gap-y-4",
+            classname,
+        )}>
+            {items.map((item , index) => (
+            <div key={getkey ? getkey(item, index) : index}>
+                {renderItems(item , index)}
+            </div>
+            ))}
+        </div>
+    );
+};
+
+interface EntityItemsProps {
+    href: string;
+    title: string;
+    subtitle?: React.ReactNode;
+    image?: React.ReactNode;
+    actions?: React.ReactNode;
+    onRemove?: () => void | Promise<void>;
+    isRemoving?: boolean;
+    className?: string;
+};
+
+export const EntityItem = ({
+    href,
+    title,
+    subtitle,
+    image,
+    actions,
+    onRemove,
+    isRemoving,
+    className,
+}: EntityItemsProps) => {
+    const handleRemove = async (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if(isRemoving){
+            return;
+        }
+        if(onRemove){
+            await onRemove();
+        }
+    }
+
+    return (
+        <Link href={href} prefetch>
+            <Card className={cn(
+                "p-4 shadow-none hover:shadow cursor-pointer",
+                isRemoving && "opacity-50 cursor-not-allowed",
+                className,
+            )}>
+                <CardContent className="flex dflex-row items-center justify-between p-0">
+                    <div className="flex items-center gap-3">
+                        {image}
+                        <div>
+                            <CardTitle className="text-base font-medium">
+                                {title}
+                            </CardTitle>
+                            {!!subtitle && (
+                                <CardDescription className="text-xs">
+                                    {subtitle}
+                                </CardDescription>
+                            )}
+                        </div>
+                    </div>
+                    {(actions || onRemove) && (
+                        <div className="flex gap-x-4 items-center">
+                            {actions}
+                            {onRemove && (
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button
+                                        size="icon"
+                                        variant="ghost"
+                                        onClick={(e) => e.stopPropagation}
+                                        >
+                                            <MoreVerticalIcon className="size-4" />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent
+                                    align="end"
+                                    onClick={(e) => e.stopPropagation}
+                                    >
+                                        <DropdownMenuItem onClick={handleRemove}>
+                                            <TrashIcon className="size-4"/>
+                                            Delete
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            )}
+                        </div>
+                    )}
+                </CardContent>
+            </Card>
+        </Link> 
+    );
 };
--- src/features/workflows/components/workflow.tsx
+++ src/features/workflows/components/workflow.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { EntityContainer, EntityHeader, EntityPagination, EntitySearch } from "@/components/entity-components";
-import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows"
+import { EmptyView, EntityContainer, EntityHeader, EntityItem, EntityList, EntityPagination, EntitySearch, ErrorView, LoadingView } from "@/components/entity-components";
+import { useCreateWorkflow, useRemoveWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows"
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
 import { useWorkflowParams } from "../hooks/use-workflows-params";
 import { useEntitySearch } from "@/hooks/use-entity-search";
+import type { Workflow } from "@/generated/prisma/client";
+import { WorkflowIcon } from "lucide-react";
+import { formatDistanceToNow} from "date-fns";
 
 export const WorkflowsSearch = () => {
     const[params , setParams] = useWorkflowParams();
@@ -26,12 +29,13 @@ export const WorkflowsSearch = () => {
 export const WorkflowsList = () => {
     const workflows = useSuspenseWorkflows();
 
-    return (
-        <div className="flex-1 flex justify-center items-center">
-            <p>
-                {JSON.stringify(workflows.data, null, 2)}
-            </p>
-        </div>
+    return(
+        <EntityList
+        items={workflows.data.items}
+        getkey={(workflow) => workflow.id}
+        renderItems={(workflow) => <WorkflowItem data={workflow}/>}
+        emptyView={<WorkflowsEmpty />}
+        />
     )
 };
 
@@ -73,12 +77,12 @@ export const WorkflowsPagination = () => {
     return(
         <EntityPagination
         disabled={workflows.isFetching}
-        totalPages={workflows.data.totalPages}
+        totalPages={workflows.data.totalCount}
         page={workflows.data.page}
         onPageChange={(page) => setParams({ ...params, page})}
         />
-    )
-}
+    );
+};
 
 
 export const WorkflowsContainer = ({
@@ -95,4 +99,70 @@ export const WorkflowsContainer = ({
             {children}
         </EntityContainer>
     );
+};
+
+export const WorkflowsLoading = () => {
+    return <LoadingView message="Loading workflows..."/>
+};
+
+export const WorkflowsError = () => {
+    return <ErrorView message="Error Loading workflows..."/>
+};
+
+export const WorkflowsEmpty = () => {
+    const router = useRouter();
+    const createWorkflow = useCreateWorkflow();
+    const { handleError , modal } = useUpgradeModal();
+
+    const handleCreate = () => {
+        createWorkflow.mutate(undefined, {
+            onError: (error) => {
+                handleError(error);
+            },
+            onSuccess: (data) =>{
+                router.push(`/workflows/&{data.id}`);
+            }
+        })
+    }
+    return (
+        <>
+        {modal}
+        <EmptyView
+        onNew={handleCreate}
+        message="No Workflows found. Get Started by creating your workflow"
+        />
+        </>
+    );
+};
+
+export const WorkflowItem = ({
+    data,
+}:{data: Workflow
+}) => {
+    const removeWorkflow = useRemoveWorkflow();
+
+    const handleRemove = () => {
+        removeWorkflow.mutate({ id: data.id});
+    }
+
+    return (
+        <EntityItem 
+        href={`/workflows/${data.id}`}
+        title={data.name}
+        subtitle={
+            <>
+            Updated {formatDistanceToNow(data.updatedAt, {addSuffix: true})}{""}
+            &bull; Created{""}
+            {formatDistanceToNow(data.createdAt, {addSuffix: true})}
+            </>
+        }
+        image={
+            <div className="size-8 flex items-center justify-center">
+                <WorkflowIcon className="size-5 text-muted-foreground"/>
+            </div>
+        }
+        onRemove={handleRemove}
+        isRemoving={removeWorkflow.isPending}
+        />
+    )
 };
